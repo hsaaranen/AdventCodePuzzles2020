323c2adc42efd593e3cf9eb4f41439e9ad044b0f
Harri Saaranen <harri.saaranen@thermofisher.com>
2021-08-12 13:30:53 +0300

    Made day one top statement and started day 9 part 2

--- AdventCode1/Program.cs
+++ AdventCode1/Program.cs
@@ -2,76 +2,67 @@
 using System.Collections.Generic;
 using System.IO;
 
-namespace AdventCode1
+var entries = GetEntries(); 
+var twoSumEntries = GetTwoSumEntries(entries);
+
+if (twoSumEntries != null)
 {
-    class Program
+    Console.WriteLine($"Correct 2 sum entries were {twoSumEntries[0]} and {twoSumEntries[1]} and multiplication is {twoSumEntries[0] * twoSumEntries[1]}");
+}
+
+var treeSumEntries = GetTreeSumEntries(entries);
+
+if (treeSumEntries != null)
+{
+    Console.WriteLine($"Correct 3 sum entries were {treeSumEntries[0]}, {treeSumEntries[1]} and {treeSumEntries[2]}  and multiplication is {treeSumEntries[0] * treeSumEntries[1] * treeSumEntries[2]}");
+}
+
+
+static int[] GetEntries()
+{
+    var reader = File.OpenText("..\\..\\..\\input.txt");
+    var entries = new List<int>();
+    string line;
+    while ((line = reader.ReadLine()) != null)
     {
+        entries.Add(int.Parse(line));
+    }
 
+    return entries.ToArray();
+}
 
-        static void Main(string[] args)
+static int[] GetTwoSumEntries(int[] entries)
+{
+    foreach (var entry1 in entries)
+    {
+        foreach (var entry2 in entries)
         {
-            var entries = GetEntries();
-            var twoSumEntries = GetTwoSumEntries(entries);
-
-            if (twoSumEntries != null)
+            if (entry1 + entry2 == 2020)
             {
-                Console.WriteLine($"Correct 2 sum entries were {twoSumEntries[0]} and {twoSumEntries[1]} and multiplication is {twoSumEntries[0] * twoSumEntries[1]}");
+                return new[] {entry1, entry2};
             }
-
-            var treeSumEntries = GetTreeSumEntries(entries);
-
-            if (treeSumEntries != null)
-            {
-                Console.WriteLine($"Correct 3 sum entries were {treeSumEntries[0]}, {treeSumEntries[1]} and {treeSumEntries[2]}  and multiplication is {treeSumEntries[0] * treeSumEntries[1] * treeSumEntries[2]}");
-            }
-
-        }
-
-        static int[] GetEntries()
-        {
-            var reader = File.OpenText("..\\..\\..\\input.txt");
-            var entries = new List<int>();
-            string line;
-            while ((line = reader.ReadLine()) != null)
-            {
-                entries.Add(int.Parse(line));
-            }
-
-            return entries.ToArray();
-        }
-
-        static int[] GetTwoSumEntries(int[] entries)
-        {
-            foreach (var entry1 in entries)
-            {
-                foreach (var entry2 in entries)
-                {
-                    if (entry1 + entry2 == 2020)
-                    {
-                        return new[] {entry1, entry2};
-                    }
-                }
-            }
-
-            return null;
-        }
-
-        static int[] GetTreeSumEntries(int[] entries)
-        {
-            foreach (var entry1 in entries)
-            {
-                foreach (var entry2 in entries)
-                {
-                    foreach (var entry3 in entries)
-                    {
-                        if (entry1 + entry2 + entry3 == 2020)
-                        {
-                            return new[] { entry1, entry2, entry3 };
-                        }
-                    }
-                }
-            }
-            return null;
         }
     }
+
+    return null;
 }
+
+static int[] GetTreeSumEntries(int[] entries)
+{
+    foreach (var entry1 in entries)
+    {
+        foreach (var entry2 in entries)
+        {
+            foreach (var entry3 in entries)
+            {
+                if (entry1 + entry2 + entry3 == 2020)
+                {
+                    return new[] { entry1, entry2, entry3 };
+                }
+            }
+        }
+    }
+    return null;
+}
+    
+
---
+++ AdventCode9/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventCode9
+{
+    class Program
+    {
+        const int preAmpleSize = 25;
+
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("..\\..\\..\\input.txt");
+            var numbers = input.Select(long.Parse).ToArray();
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                if (i >= preAmpleSize)
+                {
+                    long[] numbersToCampare = new long[preAmpleSize];
+                    Array.Copy(numbers, i - preAmpleSize, numbersToCampare, 0, preAmpleSize);
+                    bool result = IsNumberSumOfPreviousNumbers(numbers[i], numbersToCampare);
+                    if (!result)
+                    {
+                        Console.WriteLine($"{numbers[i]} is the first number that doesn't meet the criteria");
+                        var weakness = FindWeaknessFromXmas(numbers, numbers[i]);
+                        Console.WriteLine($"Weakness is {weakness}");
+                        return;
+                    }
+                }
+            }
+        }
+
+        private static long FindWeaknessFromXmas(long[] numbers, long sumToFind)
+        {
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                var sum = numbers[i];
+                var sumIndex = 0;
+                while (sum < sumToFind)
+                {
+                    sumIndex++;
+                    sum += numbers[i + sumIndex];
+                    if (sum == sumToFind)
+                    {
+                        var biggestNumber = numbers[i];
+                        var smallestNumber = numbers[i];
+                        for (int j = i; j < i + sumIndex; j++)
+                        {
+                            biggestNumber = numbers[j] > biggestNumber ? numbers[j] : biggestNumber;
+                            smallestNumber = numbers[j] < smallestNumber ? numbers[j] : smallestNumber;
+                        }
+                        return smallestNumber + biggestNumber;
+                    }
+                }
+            }
+            return -1;
+        }
+
+        private static bool IsNumberSumOfPreviousNumbers(long n, long[] numbers)
+        {
+            foreach (var rootNumber in numbers)
+            {
+                foreach (var number in numbers)
+                {
+                    if (number == rootNumber)
+                    {
+                        continue;
+                    }
+
+                    if (rootNumber + number == n)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
